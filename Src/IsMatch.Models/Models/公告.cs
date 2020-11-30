b38404dfd281fc1a8bbb4c7f89b78af9040e2e1c
IsMatch <ismatch@qq.com>
2020-11-30 22:38:03 +0800

    添加公告类

---
+++ Src/IsMatch.Models/Models/公告.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+using XCode;
+using XCode.Configuration;
+using XCode.DataAccessLayer;
+
+namespace IsMatch.Models
+{
+    /// <summary>公告</summary>
+    [Serializable]
+    [DataObject]
+    [Description("公告")]
+    [BindTable("Notice", Description = "公告", ConnName = "Conn", DbType = DatabaseType.None)]
+    public partial class Notice
+    {
+        #region 属性
+        private Int32 _ID;
+        /// <summary>编号</summary>
+        [DisplayName("编号")]
+        [Description("编号")]
+        [DataObjectField(true, true, false, 0)]
+        [BindColumn("ID", "编号", "")]
+        public Int32 ID { get => _ID; set { if (OnPropertyChanging("ID", value)) { _ID = value; OnPropertyChanged("ID"); } } }
+
+        private String _Title;
+        /// <summary>标题</summary>
+        [DisplayName("标题")]
+        [Description("标题")]
+        [DataObjectField(false, false, false, 50)]
+        [BindColumn("Title", "标题", "", Master = true)]
+        public String Title { get => _Title; set { if (OnPropertyChanging("Title", value)) { _Title = value; OnPropertyChanged("Title"); } } }
+
+        private DateTime _CreateTime;
+        /// <summary>时间</summary>
+        [DisplayName("时间")]
+        [Description("时间")]
+        [DataObjectField(false, false, false, 0)]
+        [BindColumn("CreateTime", "时间", "")]
+        public DateTime CreateTime { get => _CreateTime; set { if (OnPropertyChanging("CreateTime", value)) { _CreateTime = value; OnPropertyChanged("CreateTime"); } } }
+
+        private String _Remark;
+        /// <summary>详细信息</summary>
+        [DisplayName("详细信息")]
+        [Description("详细信息")]
+        [DataObjectField(false, false, true, 500)]
+        [BindColumn("Remark", "详细信息", "")]
+        public String Remark { get => _Remark; set { if (OnPropertyChanging("Remark", value)) { _Remark = value; OnPropertyChanged("Remark"); } } }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>获取/设置 字段值</summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        public override Object this[String name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case "ID": return _ID;
+                    case "Title": return _Title;
+                    case "CreateTime": return _CreateTime;
+                    case "Remark": return _Remark;
+                    default: return base[name];
+                }
+            }
+            set
+            {
+                switch (name)
+                {
+                    case "ID": _ID = value.ToInt(); break;
+                    case "Title": _Title = Convert.ToString(value); break;
+                    case "CreateTime": _CreateTime = value.ToDateTime(); break;
+                    case "Remark": _Remark = Convert.ToString(value); break;
+                    default: base[name] = value; break;
+                }
+            }
+        }
+        #endregion
+
+        #region 字段名
+        /// <summary>取得公告字段信息的快捷方式</summary>
+        public partial class _
+        {
+            /// <summary>编号</summary>
+            public static readonly Field ID = FindByName("ID");
+
+            /// <summary>标题</summary>
+            public static readonly Field Title = FindByName("Title");
+
+            /// <summary>时间</summary>
+            public static readonly Field CreateTime = FindByName("CreateTime");
+
+            /// <summary>详细信息</summary>
+            public static readonly Field Remark = FindByName("Remark");
+
+            static Field FindByName(String name) => Meta.Table.FindByName(name);
+        }
+
+        /// <summary>取得公告字段名称的快捷方式</summary>
+        public partial class __
+        {
+            /// <summary>编号</summary>
+            public const String ID = "ID";
+
+            /// <summary>标题</summary>
+            public const String Title = "Title";
+
+            /// <summary>时间</summary>
+            public const String CreateTime = "CreateTime";
+
+            /// <summary>详细信息</summary>
+            public const String Remark = "Remark";
+        }
+        #endregion
+    }
+}
